2fed210aec9e699f12dfeeed55ff37bcc3542258
learnsshala <144378130+learnsshala@users.noreply.github.com>
2024-03-26 18:02:18 +0530

    add

--- src/PatientTable/TableBody.js
+++ src/PatientTable/TableBody.js
@@ -6,8 +6,13 @@ const TableBody = (props) => {
     const birthYearEnd = currentYear - ageRange[0];
 
     const filteredData = data && data.filter(date => {
-        const birthYear = new Date(`${date?.resource?.birthDate}`)?.getFullYear();
+        if(date?.resource?.birthDate){
+            const birthYear = new Date(`${date?.resource?.birthDate}`)?.getFullYear();
         return birthYear >= birthYearStart && birthYear <= birthYearEnd;
+        }
+        else {
+            return data;
+        }
     });
     
     return (
